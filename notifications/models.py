c152524cfd1f74fe7991c4088f2cda0b554b30fd
serj2626 <serj2626@mail.ru>
2024-11-15 23:32:10 +0300

    feat: created new models NotificationEmployee  and NotificationCompany

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+
+
+TYPE_NOTIFICATION = (
+    ("new_vacancy", "Новая вакансия"),
+    ("new_interview", "Новый интервью"),
+    ("new_message", "Новое сообщение"),
+    ("new_response", "Новый отклик"),
+    ("archive_vacancy", "Вакансия перенесена в архив"),
+)
+
+
+class NotificationEmployee(models.Model):
+    """
+    Модель уведомлений для работников
+    """
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=255, choices=TYPE_NOTIFICATION)
+    read = models.BooleanField(default=False, verbose_name="Прочитано")
+
+
+
+class NotificationCompany(models.Model):
+    """
+    Модель уведомлений для компаний
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=255, choices=TYPE_NOTIFICATION)
+    read = models.BooleanField(default=False, verbose_name="Прочитано")
