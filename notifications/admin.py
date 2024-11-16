d99cb4d4c990cc26da51f4c90be274d8639ea363
serj2626 <serj2626@mail.ru>
2024-11-16 14:28:44 +0300

    feat: created new admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import NotificationCompany, NotificationEmployee
 
-# Register your models here.
+
+@admin.register(NotificationCompany)
+class NotificationCompanyAdmin(admin.ModelAdmin):
+    """
+    Админка уведомлений компаний
+    """
+
+    list_display = (
+        "user",
+        "message",
+        "created_at",
+        "type",
+        "read",
+    )
+
+
+@admin.register(NotificationEmployee)
+class NotificationEmployeeAdmin(admin.ModelAdmin):
+    """
+    Админка уведомлений работников
+    """
+
+    list_display = (
+        "user",
+        "message",
+        "created_at",
+        "type",
+        "read",
+    )
