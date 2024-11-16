17ef81901f1f2542139b690f606edb41c9fac0f8
serj2626 <serj2626@mail.ru>
2024-11-16 14:27:41 +0300

    refactor: update type_notification

--- notifications/models.py
+++ notifications/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
 from accounts.models import User
 
 
 TYPE_NOTIFICATION = (
     ("new_vacancy", "Новая вакансия"),
-    ("new_interview", "Новый интервью"),
+    ("new_interview", "Новое интервью"),
     ("new_message", "Новое сообщение"),
     ("new_response", "Новый отклик"),
     ("archive_vacancy", "Вакансия перенесена в архив"),
@@ -16,13 +15,19 @@ class NotificationEmployee(models.Model):
     """
     Модель уведомлений для работников
     """
-    
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     type = models.CharField(max_length=255, choices=TYPE_NOTIFICATION)
     read = models.BooleanField(default=False, verbose_name="Прочитано")
 
+    class Meta:
+        verbose_name = "Уведомление работника"
+        verbose_name_plural = "Уведомления работников"
+
+    def __str__(self):
+        return f"Уведомление работника {self.user.name}"
 
 
 class NotificationCompany(models.Model):
@@ -35,3 +40,10 @@ class NotificationCompany(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     type = models.CharField(max_length=255, choices=TYPE_NOTIFICATION)
     read = models.BooleanField(default=False, verbose_name="Прочитано")
+
+    class Meta:
+        verbose_name = "Уведомление компании"
+        verbose_name_plural = "Уведомления компаний"
+
+    def __str__(self):
+        return f"Уведомление компании {self.user.name}"
