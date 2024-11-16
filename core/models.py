86f729f2172336e329212b95fb36a3ec1a7a61fd
serj2626 <serj2626@mail.ru>
2024-11-16 14:26:56 +0300

    feat: new models Notification for employee and company

--- core/models.py
+++ core/models.py
@@ -214,3 +214,45 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Комментарий от {self.user.email}"
+
+
+class FavoriteResume(models.Model):
+    """
+    Модель избранных резюме  для компании.
+    """
+
+    resume = models.ForeignKey(
+        Resume, on_delete=models.SET_NULL, null=True, verbose_name="Резюме"
+    )
+    company = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Компания")
+    created_at = models.DateTimeField("Создан", auto_now_add=True)
+    updated_at = models.DateTimeField("Обновлен", auto_now=True)
+
+    def __str__(self):
+        return f"Избранное резюме {self.resume} для компании {self.company}"
+
+    class Meta:
+        verbose_name = "Избранные резюме"
+        verbose_name_plural = "Избранные резюме"
+
+
+class FavoriteVacancy(models.Model):
+    """
+    Модель избранных вакансий для работника.
+    """
+
+    vacancy = models.ForeignKey(
+        Vacancy, on_delete=models.SET_NULL, null=True, verbose_name="Вакансия"
+    )
+    employee = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Работник"
+    )
+    created_at = models.DateTimeField("Создан", auto_now_add=True)
+    updated_at = models.DateTimeField("Обновлен", auto_now=True)
+
+    def __str__(self):
+        return f"Избранная вакансия {self.vacancy} для работника {self.employee}"
+
+    class Meta:
+        verbose_name = "Избранные вакансии"
+        verbose_name_plural = "Избранные вакансии"
