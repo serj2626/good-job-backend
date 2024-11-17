ff1dc9266c2596bf93d097ad38ea70c9812e2301
serj2626 <serj2626@mail.ru>
2024-11-17 16:07:51 +0300

    feat: new file with global constant type

---
+++ common/const.py
@@ -0,0 +1,38 @@
+WORK_SCHEDULE = (
+    ("full-time", "Полный рабочий день"),
+    ("part-time", "Частичная занятость"),
+    ("remote", "Удаленная работа"),
+)
+
+LEVELS_REQUIREMENTS = (
+    ("intern", "Intern"),
+    ("junior", "Junior"),
+    ("junior_plus", "Junior+"),
+    ("middle", "Middle"),
+    ("middle_plus", "Middle+"),
+    ("senior", "Senior"),
+    ("team_lead", "Team Lead"),
+)
+
+STATUS_VACANCY = (
+    ("open", " Открыта"),
+    ("archived", "Архив"),
+)
+
+
+CATEGORY_TYPES = (
+    ("backend", "Бэкенд"),
+    ("frontend", "Фронтенд"),
+    ("fullstack", "Фулстайк"),
+    ("analytics", "Аналитика"),
+    ("devops", "DevOps"),
+    ("design", "Дизайн"),
+    ("other", "Другое"),
+)
+
+
+USER_TYPES = (
+    ("Company", "Компания"),
+    ("Employee", "Работник"),
+    ("Admin", "Администратор"),
+)
