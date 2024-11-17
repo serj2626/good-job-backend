c3cf03f17b1e777ea47252378a1a980bfd1ef359
serj2626 <serj2626@mail.ru>
2024-11-17 15:50:59 +0300

    feat: new views for favorite

--- core/views.py
+++ core/views.py
@@ -5,10 +5,21 @@ from .serializers import (
     CommentSerializer,
     ExperienceSerializer,
     StackSerializer,
+    FavoriteResumeSerializer,
+    FavoriteVacancySerializer,
 )
 from drf_spectacular.utils import extend_schema
 
-from .models import Resume, Vacancy, Category, Comment, Experience, Stack
+from .models import (
+    Resume,
+    Vacancy,
+    Category,
+    Comment,
+    Experience,
+    Stack,
+    FavoriteResume,
+    FavoriteVacancy,
+)
 from rest_framework import generics
 
 
@@ -236,3 +247,127 @@ class StackListView(generics.ListAPIView):
     )
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
+
+
+class FavoriteResumeListView(generics.ListCreateAPIView):
+    serializer_class = FavoriteResumeSerializer
+    queryset = FavoriteResume.objects.all()
+
+    @extend_schema(
+        tags=["Избранные резюме"],
+        request=FavoriteResumeSerializer,
+        responses=FavoriteResumeSerializer,
+        summary="Добавление избранного резюме",
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=["Избранные резюме"],
+        responses=FavoriteResumeSerializer,
+        summary="Получение всех избранных резюме",
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+
+class FavoriteResumeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = FavoriteResumeSerializer
+    queryset = FavoriteResume.objects.all()
+
+    @extend_schema(
+        tags=["Избранные резюме"],
+        responses=FavoriteResumeSerializer,
+        summary="Получение избранного резюме по id",
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=["Избранные резюме"],
+        request=FavoriteResumeSerializer,
+        responses=FavoriteResumeSerializer,
+        summary="Обновление избранного резюме по id",
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=["Избранные резюме"],
+        request=FavoriteResumeSerializer,
+        responses=FavoriteResumeSerializer,
+        summary="Удаление избранного резюме по id",
+    )
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=["Избранные резюме"],
+        request=FavoriteResumeSerializer,
+        responses=FavoriteResumeSerializer,
+        summary="Частичное обновление избранного резюме по id",
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+
+class FavoriteVacancyListView(generics.ListCreateAPIView):
+    serializer_class = FavoriteVacancySerializer
+    queryset = FavoriteVacancy.objects.all()
+
+    @extend_schema(
+        tags=["Избранные вакансии"],
+        request=FavoriteVacancySerializer,
+        responses=FavoriteVacancySerializer,
+        summary="Добавление избранной вакансии",
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=["Избранные вакансии"],
+        responses=FavoriteVacancySerializer,
+        summary="Получение всех избранных вакансий",
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+
+class FavoriteVacancyDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = FavoriteVacancySerializer
+    queryset = FavoriteVacancy.objects.all()
+
+    @extend_schema(
+        tags=["Избранные вакансии"],
+        responses=FavoriteVacancySerializer,
+        summary="Получение избранной вакансии по id",
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=["Избранные вакансии"],
+        request=FavoriteVacancySerializer,
+        responses=FavoriteVacancySerializer,
+        summary="Обновление избранной вакансии по id",
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=["Избранные вакансии"],
+        request=FavoriteVacancySerializer,
+        responses=FavoriteVacancySerializer,
+        summary="Удаление избранной вакансии по id",
+    )
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=["Избранные вакансии"],
+        request=FavoriteVacancySerializer,
+        responses=FavoriteVacancySerializer,
+        summary="Частичное обновление избранной вакансии по id",
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
