236252c489b074a1e326109be6b5275c8efcd9c7
serj2626 <serj2626@mail.ru>
2024-11-19 16:27:51 +0300

    feat: update path to image

--- common/service.py
+++ common/service.py
@@ -4,4 +4,4 @@ def get_clear_slug(email: str) -> str:
 
 
 def get_path_for_avatar(instance, filename) -> str:
-    return f"avatars/{instance.user.type}/{instance.user.email}/{filename}"
+    return f"resumes/{instance.employee.user.email}/{instance.id}/{filename}"
