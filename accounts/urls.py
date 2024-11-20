0ea7db9441a79010b531306773c0e243df2f4d36
serj2626 <serj2626@mail.ru>
2024-11-20 11:59:42 +0300

    feat: added path for user info

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import RegisterView
+from .views import RegisterView, get_user_info
 
 urlpatterns = [
+    path("user-info/", get_user_info, name="get_user_info"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", RegisterView.as_view(), name="register"),
