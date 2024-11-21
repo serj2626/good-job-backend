801bbdb5c0b00ae721549daa6efd09f5c0dd2f95
serj2626 <serj2626@mail.ru>
2024-11-21 20:23:40 +0300

    refactor: added summary in functions

--- common/service.py
+++ common/service.py
@@ -1,10 +1,18 @@
 def get_clear_slug(email: str) -> str:
+    """
+    Функция возвращает чистый slug без домена
+
+    :param email: email
+    :return: clear slug
+    """
     indx = email.find("@")
     return email[:indx]
 
 
 def get_path_for_avatar(instance, filename) -> str:
     """
+    Функция возвращает путь к аватару
+
     :param instance: user
     :param filename: filename
     :return: path
@@ -13,8 +21,34 @@ def get_path_for_avatar(instance, filename) -> str:
     return f"resumes/{instance.employee.user.email}/{instance.id}/{filename}"
 
 
+def get_path_for_avatar_employee(instance, filename) -> str:
+    """
+    Функция возвращает путь к аватару работника
+
+    :param instance: employee
+    :param filename: filename
+    :return: path
+    """
+
+    return f"employees/{instance.user.email}/{filename}"
+
+
+def get_path_for_avatar_company(instance, filename) -> str:
+    """
+    Функция возвращает путь к аватару компании
+
+    :param instance: company
+    :param filename: filename
+    :return: path
+    """
+
+    return f"company/{instance.user.email}/{instance.name}/{filename}"
+
+
 def get_path_for_photo_feedback(instance, filename) -> str:
     """
+    Функция возвращает путь к фото обратной связи
+
     :param instance: feedback
     :param filename: filename
     :return: path
@@ -25,6 +59,8 @@ def get_path_for_photo_feedback(instance, filename) -> str:
 
 def get_path_for_image_project(instance, filename) -> str:
     """
+    Функция возвращает путь к фото проекта
+
     :param instance: project
     :param filename: filename
     :return: path
