af8baa4c17a297dfda61865ef590a4db8c35f6d5
serj2626 <serj2626@mail.ru>
2024-11-27 22:43:58 +0300

    feat: add field middle name

--- employees/models.py
+++ employees/models.py
@@ -31,6 +31,9 @@ class Employee(ProfileModel):
     last_name = models.CharField(
         max_length=255, blank=True, null=True, verbose_name="Фамилия"
     )
+    middle_name = models.CharField(
+        max_length=255, blank=True, null=True, verbose_name="Отчество"
+    )
 
     gender = models.CharField(
         max_length=255, choices=TYPE_GENDER, default="other", verbose_name="Пол"
