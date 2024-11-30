682a75d83cc00965a20350930867fca0f37989f0
serj2626 <serj2626@mail.ru>
2024-11-30 13:37:06 +0300

    feat: updated mtrhod clean

--- common/models.py
+++ common/models.py
@@ -1,8 +1,9 @@
+from locale import currency
 from django.db import models
 import uuid
 from django.core.exceptions import ValidationError
 from core.models import Category, Stack
-from common.const import WORK_SCHEDULE
+from common.const import CURRENCY_TYPE, WORK_SCHEDULE
 from common.service import get_clear_slug
 from django.utils.timesince import timesince
 
@@ -42,10 +43,11 @@ class ResumeOrVacancyModel(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, verbose_name="Категория"
     )
-    min_salary = models.SmallIntegerField("Минимальная зарплата", blank=True, null=True)
+    min_salary = models.SmallIntegerField("Минимальная зарплата", null=True)
     max_salary = models.SmallIntegerField(
         "Максимальная зарплата", blank=True, null=True
     )
+    currency = models.CharField("Валюта", max_length=200, choices=CURRENCY_TYPE, default="RUB")
     work_schedule = models.CharField(
         "График работы", max_length=200, choices=WORK_SCHEDULE, default="full-time"
     )
@@ -57,9 +59,7 @@ class ResumeOrVacancyModel(models.Model):
         abstract = True
 
     def clean(self):
-        if not self.min_salary and self.max_salary:
-            raise ValidationError("Необходимо указать минимальную зарплату")
-        elif self.min_salary and self.max_salary and self.min_salary > self.max_salary:
+        if self.min_salary and self.max_salary and self.min_salary > self.max_salary:
             raise ValidationError(
                 "Минимальная зарплата не может быть больше максимальной"
             )
