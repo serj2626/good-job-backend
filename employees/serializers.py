39e4f32209e75e72aa057cfb0451d315321da6c5
serj2626 <serj2626@mail.ru>
2024-11-30 13:37:55 +0300

    feat: create new  serializer EmployeeDetailSerializer

--- employees/serializers.py
+++ employees/serializers.py
@@ -46,11 +46,35 @@ class ExperienceSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class EmployeeDetailSerializer(serializers.ModelSerializer):
+    user = UserDataSerializer()
+    stacks = serializers.SlugRelatedField(many=True, read_only=True, slug_field="name")
+    projects = ProjectSerializer(many=True)
+    gender = serializers.CharField(source="get_gender_display")
+    experiences = ExperienceSerializer(many=True)
+    educations = EducationSerializer(many=True)
+    age = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Employee
+        fields = "__all__"
+
+    def get_age(self, obj):
+        return (
+            None
+            if not obj.date_of_birth
+            else date.today().year - obj.date_of_birth.year
+        )
+
+
 class ResumeSerializer(serializers.ModelSerializer):
-    projects = ProjectSerializer(source="employee.projects", many=True)
     stacks = serializers.SlugRelatedField(many=True, read_only=True, slug_field="name")
     category = serializers.CharField(source="category.name")
     full_name = serializers.SerializerMethodField()
+    educations = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="title"
+    )
+    employee = EmployeeDetailSerializer()
 
     def get_full_name(self, obj):
         return obj.employee.first_name + " " + obj.employee.last_name
