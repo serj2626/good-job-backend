d87a7ae0662a4d60ec3ac172e8c6d87b94130ba9
serj2626 <serj2626@mail.ru>
2024-12-01 20:35:10 +0300

    feat: new funcs for paths docs

--- common/service.py
+++ common/service.py
@@ -67,3 +67,69 @@ def get_path_for_image_project(instance, filename) -> str:
     """
 
     return f"projects/{instance.employee.user.email}/{filename}"
+
+
+def get_path_for_check_company_egrul(instance, filename):
+    """
+    Функция возвращает путь к выписке из ЕГРЮЛ и ЕГРИП
+
+    :param instance: company
+    :param filename: filename
+    :return: path
+    """
+    return f"company/{instance.company.slug}/docs/egrul/{filename}"
+
+
+def get_path_for_check_company_certificate(instance, filename):
+    """
+    Функция возвращает путь к Cвидетельство о регистрации фирмы или ИП
+
+    :param instance: company
+    :param filename: filename
+    :return: path
+    """
+    return f"company/{instance.company.slug}/docs/certificate/{filename}"
+
+
+def get_path_for_check_company_certificate_nalog(instance, filename):
+    """
+    Функция возвращает путь к Cправка о постановке на учет в налоговом органе
+
+    :param instance: company
+    :param filename: filename
+    :return: path
+    """
+    return f"company/{instance.company.slug}/docs/certificate_nalog/{filename}"
+
+
+def get_path_for_check_company_constitution(instance, filename):
+    """
+    Функция возвращает путь к Устав
+
+    :param instance: company
+    :param filename: filename
+    :return: path
+    """
+    return f"company/{instance.company.slug}/docs/constitution/{filename}"
+
+
+def get_path_for_check_company_license(instance, filename):
+    """
+    Функция возвращает путь к Лицензия на необходимые виды деятельности
+
+    :param instance: company
+    :param filename: filename
+    :return: path
+    """
+    return f"company/{instance.company.slug}/docs/license/{filename}"
+
+
+def get_path_for_check_company_no_debt(instance, filename):
+    """
+    Функция возвращает путь к Справка, подтверждающая отсутствие долгов по налогам и другим платежам
+
+    :param instance: company
+    :param filename: filename
+    :return: path
+    """
+    return f"company/{instance.company.slug}/docs/no_debt/{filename}"
